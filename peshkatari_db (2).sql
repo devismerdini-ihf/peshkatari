4b0031d10674207bb97f7054dc712e098025f15b
Devis Merdini <devismerdini@hotmail.com>
2025-11-27 14:30:43 +0100

    modified:   css/style.css 	modified:   peshkatari_db (2).sql 	modified:   php/connection/connect.php

--- peshkatari_db (2).sql
+++ peshkatari_db (2).sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `peshkatari_db`
+-- Database: `peshkatari_db2`
 --
 
 -- --------------------------------------------------------
--- php/connection/connect.php
+++ php/connection/connect.php
@@ -2,7 +2,7 @@
 $hostname = "localhost";
 $serverusername = "root";
 $serverpassword = "";
-$database = "peshkatari_db";
+$database = "peshkatari_db2";
 $connect = mysqli_connect($hostname, $serverusername, $serverpassword, $database);
 if (!$connect) {
     die("Connection was not successful");
